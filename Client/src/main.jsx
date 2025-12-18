8c6c93b851cb9b659e5ca54fcbd40b3662288bd7
Nitesh Kumar <kumar183nitesh@gmail.com>
2025-12-19 03:31:34 +0530

    test4

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -19,27 +19,22 @@
 //   </ClerkProvider>
 // );
 
-// import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-// import { ClerkProvider } from "@clerk/clerk-react";
-// import App from "./App.jsx";
-// import "./index.css";
-
-// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-// if (!PUBLISHABLE_KEY) {
-//   throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
-// }
-
-// createRoot(document.getElementById("root")).render(
-//   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </ClerkProvider>
-// );
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/clerk-react";
+import App from "./App.jsx";
+import "./index.css";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
+}
 
 createRoot(document.getElementById("root")).render(
-  <h1 style={{ padding: 40, fontSize: 30 }}>REACT IS WORKING</h1>
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 );
