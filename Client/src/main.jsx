e6fe5e7a7469cbaa37f13951ba24b0655b973aa9
Nitesh Kumar <kumar183nitesh@gmail.com>
2025-12-19 03:19:08 +0530

    test1

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -19,22 +19,27 @@
 //   </ClerkProvider>
 // );
 
+// import { createRoot } from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import { ClerkProvider } from "@clerk/clerk-react";
+// import App from "./App.jsx";
+// import "./index.css";
+
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// if (!PUBLISHABLE_KEY) {
+//   throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
+// }
+
+// createRoot(document.getElementById("root")).render(
+//   <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </ClerkProvider>
+// );
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { ClerkProvider } from "@clerk/clerk-react";
-import App from "./App.jsx";
-import "./index.css";
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
-}
 
 createRoot(document.getElementById("root")).render(
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ClerkProvider>
+  <h1 style={{ padding: 40, fontSize: 30 }}>REACT IS WORKING</h1>
 );
