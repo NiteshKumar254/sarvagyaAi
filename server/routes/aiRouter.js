0c46de5e4457110bc4e12f0e6bf5c040d2e41578
Nitesh Kumar <kumar183nitesh@gmail.com>
2025-12-19 04:22:29 +0530

    done

--- server/routes/aiRouter.js
+++ server/routes/aiRouter.js
@@ -1,50 +1,50 @@
-// import express from "express";
-// import { auth } from "../middleware/auth.js";
-// import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
-// import { upload } from "../configs/multer.js";
-// const aiRouter = express.Router();
-
-// // aiRouter.post('/generate-article', auth, generateArticle)
-// aiRouter.post("/write-article", auth, generateArticle);
-
-// aiRouter.post('/generate-blog-title', auth, generateBlogTitle)
-// aiRouter.post('/generate-image', auth, generateImage)
-// aiRouter.post('/remove-image-background', upload.single('image'), auth, removeImageBackground)
-// aiRouter.post('/remove-image-object', upload.single('image'), auth, removeImageObject)
-// aiRouter.post('/resume-review', upload.single('resume'), auth, resumeReview)
-
-// export default aiRouter;
 import express from "express";
-import {
-  generateArticle,
-  generateBlogTitle,
-  generateImage,
-  removeImageBackground,
-  removeImageObject,
-  resumeReview
-} from "../controllers/aiController.js";
+import { auth } from "../middleware/auth.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject, resumeReview } from "../controllers/aiController.js";
 import { upload } from "../configs/multer.js";
-
 const aiRouter = express.Router();
 
-aiRouter.post("/write-article", generateArticle);
+// aiRouter.post('/generate-article', auth, generateArticle)
+aiRouter.post("/write-article", auth, generateArticle);
 
-aiRouter.post("/generate-blog-title", generateBlogTitle);
-aiRouter.post("/generate-image", generateImage);
-aiRouter.post(
-  "/remove-image-background",
-  upload.single("image"),
-  removeImageBackground
-);
-aiRouter.post(
-  "/remove-image-object",
-  upload.single("image"),
-  removeImageObject
-);
-aiRouter.post(
-  "/resume-review",
-  upload.single("resume"),
-  resumeReview
-);
+aiRouter.post('/generate-blog-title', auth, generateBlogTitle)
+aiRouter.post('/generate-image', auth, generateImage)
+aiRouter.post('/remove-image-background', upload.single('image'), auth, removeImageBackground)
+aiRouter.post('/remove-image-object', upload.single('image'), auth, removeImageObject)
+aiRouter.post('/resume-review', upload.single('resume'), auth, resumeReview)
 
 export default aiRouter;
+// import express from "express";
+// import {
+//   generateArticle,
+//   generateBlogTitle,
+//   generateImage,
+//   removeImageBackground,
+//   removeImageObject,
+//   resumeReview
+// } from "../controllers/aiController.js";
+// import { upload } from "../configs/multer.js";
+
+// const aiRouter = express.Router();
+
+// aiRouter.post("/write-article", generateArticle);
+
+// aiRouter.post("/generate-blog-title", generateBlogTitle);
+// aiRouter.post("/generate-image", generateImage);
+// aiRouter.post(
+//   "/remove-image-background",
+//   upload.single("image"),
+//   removeImageBackground
+// );
+// aiRouter.post(
+//   "/remove-image-object",
+//   upload.single("image"),
+//   removeImageObject
+// );
+// aiRouter.post(
+//   "/resume-review",
+//   upload.single("resume"),
+//   resumeReview
+// );
+
+// export default aiRouter;
